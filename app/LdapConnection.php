a37edc1da1b5fdaebec219dfe32d7e7999d5a118
Steve Bauman <steven_bauman@outlook.com>
2019-09-13 18:53:43 -0400

    Added casts and encrypt username and password attributes

--- app/LdapConnection.php
+++ app/LdapConnection.php
@@ -14,4 +14,36 @@ class LdapConnection extends Model
     const STATUS_ONLINE = 1;
     const STATUS_OFFLINE = 2;
     const STATUS_INVALID_CREDENTIALS = 3;
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'hosts' => 'array',
+        'use_ssl' => 'bool',
+        'use_tls' => 'bool',
+        'follow_referrals' => 'bool',
+    ];
+
+    /**
+     * Encrypts the username upon it being set.
+     *
+     * @param string $username
+     */
+    public function setUsernameAttribute($username)
+    {
+        $this->attributes['username'] = encrypt($username);
+    }
+
+    /**
+     * Encrypts the password upon it being set.
+     *
+     * @param string $password
+     */
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password'] = encrypt($password);
+    }
 }

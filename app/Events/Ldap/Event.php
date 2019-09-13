a8c01591beb46b7e5a7adafab4528ae98fd2584d
Steve Bauman <steven_bauman@outlook.com>
2019-09-13 19:19:35 -0400

    Updated Ldap event.
    Accept the change, object and attribute name

--- app/Events/Ldap/Event.php
+++ app/Events/Ldap/Event.php
@@ -3,9 +3,18 @@
 namespace App\Events\Ldap;
 
 use App\LdapObject;
+use App\LdapChange;
+use Illuminate\Support\Arr;
 
 abstract class Event
 {
+    /**
+     * The LDAP change record.
+     *
+     * @var LdapChange
+     */
+    public $change;
+
     /**
      * The LDAP object that was changed.
      *
@@ -16,31 +25,73 @@ abstract class Event
     /**
      * The new value of the LDAP change.
      *
-     * @var array
+     * @var string
      */
-    public $value;
+    public $attribute;
 
     /**
      * Constructor.
      *
+     * @param LdapChange $change
      * @param LdapObject $object
-     * @param array      $value
+     * @param array      $attribute
      */
-    public function __construct(LdapObject $object, array $value = [])
+    public function __construct(LdapChange $change, LdapObject $object, $attribute)
     {
+        $this->change = $change;
         $this->object = $object;
-        $this->value = $value;
+        $this->attribute = $attribute;
     }
 
     /**
-     * Returns the subject of the event.
+     * Get the common name from the LDAP object.
+     *
+     * @return string
+     */
+    public function getObjectName()
+    {
+        return Arr::get($this->object->attributes, 'cn.0');
+    }
+
+    /**
+     * Returns the objects (current) distinguished name.
+     *
+     * @return string
+     */
+    public function getObjectDn()
+    {
+        return $this->object->dn;
+    }
+
+    /**
+     * Get the value of the changed attribute.
+     *
+     * @return mixed
+     */
+    public function getValue()
+    {
+        return $this->change->attributes[$this->attribute];
+    }
+
+    /**
+     * Get the first value of the changed attribute.
+     *
+     * @return string
+     */
+    public function getFirstValue()
+    {
+        return Arr::first($this->getValue());
+    }
+
+    /**
+     * Get the subject of the event.
      *
      * @return string
      */
     abstract public function getSubject() : string;
 
     /**
-     * Returns the full description of the event.
+     * Get the full description of the event.
      *
      * @return string
      */

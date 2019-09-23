63064e2145f1de7b9c7b94cb83a781dde5e808a0
Steve Bauman <steven_bauman@outlook.com>
2019-09-22 21:39:23 -0400

    Added dates attribute

--- app/LdapChange.php
+++ app/LdapChange.php
@@ -15,7 +15,15 @@ class LdapChange extends Model
         'attribute',
         'before',
         'after',
+        'ldap_updated_at',
     ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['ldap_updated_at'];
 
     /**
      * The attributes that should be cast to native types.

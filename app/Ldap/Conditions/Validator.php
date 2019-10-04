44d962c638b25a9043a7bdbc1b9f1db6042a88e6
Steve Bauman <steven_bauman@outlook.com>
2019-10-04 12:12:57 -0400

    Moved condition validation into Validator class

---
+++ app/Ldap/Conditions/Validator.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Ldap\Conditions;
+
+use App\LdapNotifierCondition;
+use Illuminate\Support\Collection;
+use App\Ldap\Transformers\AttributeTransformer;
+
+class Validator
+{
+    /**
+     * The conditions map.
+     *
+     * @var array
+     */
+    protected $map = [
+        LdapNotifierCondition::OPERATOR_HAS => Has::class,
+        LdapNotifierCondition::OPERATOR_PAST => IsPast::class,
+        LdapNotifierCondition::OPERATOR_EQUALS => Equals::class,
+        LdapNotifierCondition::OPERATOR_CONTAINS => Contains::class,
+    ];
+
+    /**
+     * The conditions to validate against.
+     *
+     * @var Collection
+     */
+    protected $conditions;
+
+    /**
+     * The value to validate.
+     *
+     * @var array
+     */
+    protected $value;
+
+    /**
+     * Constructor.
+     *
+     * @param Collection $conditions
+     * @param array      $value
+     */
+    public function __construct(Collection $conditions, array $value = [])
+    {
+        $this->conditions = $conditions;
+        $this->value = $value;
+    }
+
+    /**
+     * Determine if the conditions pass.
+     *
+     * @return bool
+     */
+    public function passes()
+    {
+        return $this->conditions->filter(function (LdapNotifierCondition $condition) {
+                // Create the conditions validator and determine if it passes.
+                return transform($this->map[$condition->operator], function ($class) use ($condition) {
+                    return new $class(
+                        $this->getTransformedValue($condition),
+                        $condition->attribute,
+                        $condition->operator,
+                        $condition->value
+                    );
+                })->passes();
+            })->count() == $this->conditions->count();
+    }
+
+    /**
+     * Get the transformed changed value.
+     *
+     * @param LdapNotifierCondition $condition
+     *
+     * @return mixed
+     */
+    protected function getTransformedValue(LdapNotifierCondition $condition)
+    {
+        return $this->transformChangedValue([
+            $condition->attribute => $this->value
+        ])[$condition->attribute];
+    }
+
+    /**
+     * Transform the changed value for conditional checking.
+     *
+     * @param array $value
+     *
+     * @return array|mixed
+     */
+    protected function transformChangedValue(array $value)
+    {
+        return (new AttributeTransformer($value))->transform();
+    }
+}
--- app/Observers/LdapChangeObserver.php
+++ app/Observers/LdapChangeObserver.php
@@ -4,28 +4,11 @@ namespace App\Observers;
 
 use App\LdapChange;
 use App\LdapNotifier;
-use App\Ldap\Conditions\Has;
-use App\LdapNotifierCondition;
-use App\Ldap\Conditions\Equals;
-use App\Ldap\Conditions\IsPast;
-use App\Ldap\Conditions\Contains;
+use App\Ldap\Conditions\Validator;
 use App\Notifications\LdapObjectHasChanged;
-use App\Ldap\Transformers\AttributeTransformer;
 
 class LdapChangeObserver
 {
-    /**
-     * The conditions map.
-     *
-     * @var array
-     */
-    protected $map = [
-        LdapNotifierCondition::OPERATOR_HAS => Has::class,
-        LdapNotifierCondition::OPERATOR_PAST => IsPast::class,
-        LdapNotifierCondition::OPERATOR_EQUALS => Equals::class,
-        LdapNotifierCondition::OPERATOR_CONTAINS => Contains::class,
-    ];
-
     /**
      * Fire the relevant events upon LDAP change creation.
      *
@@ -59,43 +42,7 @@ class LdapChangeObserver
      */
     protected function passesConditions($conditions, LdapChange $change)
     {
-        return $conditions->filter(function (LdapNotifierCondition $condition) use ($change) {
-            // Create the conditions validator and determine if it passes.
-            return transform($this->map[$condition->operator], function ($class) use ($change, $condition) {
-                return new $class(
-                    $this->getTransformedValue($change),
-                    $condition->attribute,
-                    $condition->operator,
-                    $condition->value
-                );
-            })->passes();
-        })->count() == $conditions->count();
-    }
-
-    /**
-     * Get the transformed changed value.
-     *
-     * @param LdapChange $change
-     *
-     * @return mixed
-     */
-    protected function getTransformedValue(LdapChange $change)
-    {
-        return $this->transformChangedValue([
-            $change->attribute => $change->after
-        ])[$change->attribute];
-    }
-
-    /**
-     * Transform the changed value for conditional checking.
-     *
-     * @param array $value
-     *
-     * @return array|mixed
-     */
-    protected function transformChangedValue(array $value)
-    {
-        return (new AttributeTransformer($value))->transform();
+        return (new Validator($conditions, $change->after ?? []))->passes();
     }
 
     /**

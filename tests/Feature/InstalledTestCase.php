a6e23fc1d232a635b5495273bf4e11c34f0c3df6
Steve Bauman <steven_bauman@outlook.com>
2019-10-07 15:00:19 -0400

    Added installed test case

---
+++ tests/Feature/InstalledTestCase.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature;
+
+use Mockery as m;
+use Tests\TestCase;
+use App\Installer\Installer;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class InstalledTestCase extends TestCase
+{
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $installer = m::mock(Installer::class);
+        $installer->shouldReceive('installed')->andReturnTrue();
+
+        $this->app->instance(Installer::class, $installer);
+    }
+}

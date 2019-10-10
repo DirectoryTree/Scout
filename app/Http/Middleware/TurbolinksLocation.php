dc57e9126109a0c6b198a0b783d45c1532d5de2b
Steve Bauman <steven_bauman@outlook.com>
2019-10-10 16:59:39 -0400

    Added turbolinks location middleware

---
+++ app/Http/Middleware/TurbolinksLocation.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Symfony\Component\HttpFoundation\StreamedResponse;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+
+class TurbolinksLocation
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param Closure                  $next
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+
+        if ($response instanceof BinaryFileResponse || $response instanceof StreamedResponse) {
+            return $response;
+        }
+
+        return $response->header('Turbolinks-Location', $request->url());
+    }
+}

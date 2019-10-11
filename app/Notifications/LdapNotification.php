71b248932632abb65824b110f1e80d2e80fcc195
Steve Bauman <steven_bauman@outlook.com>
2019-10-11 16:51:42 -0400

    Fully working notificatons :fire:

--- app/Notifications/LdapNotification.php
+++ app/Notifications/LdapNotification.php
@@ -4,6 +4,7 @@ namespace App\Notifications;
 
 use App\LdapObject;
 use App\LdapNotifier;
+use Illuminate\Support\Arr;
 use Illuminate\Notifications\Notification;
 
 class LdapNotification extends Notification
@@ -53,10 +54,64 @@ class LdapNotification extends Notification
      */
     public function toArray($notifiable)
     {
+        $attributes = $this->notifier->conditions->pluck('attribute')->transform(function ($attribute) {
+            return [
+                $attribute => [
+                    'original' => $this->getOriginalValue($attribute),
+                    'updated' => $this->getUpdatedValue($attribute),
+                ]
+            ];
+        });
+
         return [
             'on' => $this->object->name,
             'name' => $this->notifier->name,
             'notifiable_name' => $this->notifier->notifiable_name,
+            'attributes' => $attributes,
         ];
+    }
+
+    /**
+     * Get the LDAP objects original value for the attribute.
+     *
+     * @param string $key
+     *
+     * @return mixed
+     */
+    protected function getOriginalValue($key)
+    {
+        return Arr::get($this->getOriginalValues(), $key);
+    }
+
+    /**
+     * Get the LDAP objects updated value for the attribute.
+     *
+     * @param string $key
+     *
+     * @return mixed
+     */
+    protected function getUpdatedValue($key)
+    {
+        return Arr::get($this->getUpdatedValues(), $key);
+    }
+
+    /**
+     * Get the LDAP objects original values.
+     *
+     * @return array
+     */
+    protected function getOriginalValues()
+    {
+        return json_decode($this->object->getOriginal('values'), true);
+    }
+
+    /**
+     * Get the LDAP objects updated values.
+     *
+     * @return array
+     */
+    protected function getUpdatedValues()
+    {
+        return $this->object->getAttribute('values');
     }
 }

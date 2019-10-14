7a07b2dbd31bcc4ce8b0f01b9f52260c2c1ff08b
Steve Bauman <steven_bauman@outlook.com>
2019-10-14 15:39:06 -0400

    Use stimulus to visit notifications

--- resources/views/notifications/notification.blade.php
+++ resources/views/notifications/notification.blade.php
@@ -1,10 +1,11 @@
-<a
-    href="#"
-    class="dropdown-item py-2 {{ $notification->read() ? 'bg-light' : null }}"
+<div
+    class="dropdown-item notification py-2 {{ $notification->read() ? 'bg-light' : null }}"
     data-target="notifications.notification"
+    data-action="click->notifications#view"
     data-read="{{ $notification->read() }}"
+    data-notifications-url="{{ route('notifications.show', $notification) }}"
 >
     @include('domains.objects.partials.badge', ['object' => $notification->notifiable])
 
     <small>{{ $notification->data['name'] }}</small>
-</a>
+</div>

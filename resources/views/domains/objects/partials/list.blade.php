4f433e9ba3908f2e963c3a733c23ae23d98425ee
Steve Bauman <steven_bauman@outlook.com>
2019-10-23 17:08:56 -0400

    Unify layout and look for object list

--- resources/views/domains/objects/partials/list.blade.php
+++ resources/views/domains/objects/partials/list.blade.php
@@ -1,27 +1,43 @@
 <div class="list-group list-group-flush">
     @forelse($objects as $object)
         <div class="list-group-item {{ $object->trashed() ? 'bg-light' : null }}">
-            <div class="d-flex justify-content-start align-items-center">
-                @include('domains.objects.partials.icon')
+            <div class="d-flex justify-content-between align-items-center">
+                <div class="mr-2">
+                    <h5>
+                        @include('domains.objects.partials.badge')
 
-                <a href="{{ route('domains.objects.show', [$domain, $object]) }}" class="ml-1 font-weight-bold">
-                    {{ $object->name }}
-                </a>
+                        @if($object->children_count)
+                            <span class="ml-2 badge badge-light border" title="{{ $object->children_count }} nested">
+                                {{ $object->children_count }}
+                            </span>
+                        @endif
+                    </h5>
 
-                @if($object->children_count)
-                    <span class="ml-2 badge badge-light border">
-                        {{ $object->children_count }}
-                    </span>
-                @endif
+                    <div class="row">
+                        <div class="col text-muted small">
+                            {{ $object->dn }}
+                        </div>
+                    </div>
+                </div>
 
-                @if($object->trashed())
-                    <small class="text-muted">(deleted)</small>
-                @endif
-            </div>
+                <div class="dropdown">
+                    <button class="btn btn-sm btn-light rounded-pill border" type="button" id="btn-object-settings" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i class="fas fa-ellipsis-v"></i>
+                    </button>
 
-            <div class="row">
-                <div class="col text-muted small">
-                    {{ $object->dn }}
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="btn-object-settings">
+                        <a href="#" class="dropdown-item">
+                            <i class="far fa-bell"></i> Notifications
+                        </a>
+
+                        <a href="{{ route('domains.objects.attributes.index', [$domain, $object]) }}" class="dropdown-item">
+                            <i class="fa fa-list-ul"></i> Attributes
+                        </a>
+
+                        <a href="{{ route('domains.objects.changes.index', [$domain, $object]) }}" class="dropdown-item">
+                            <i class="fa fa-sync"></i> Changes
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>

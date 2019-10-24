a2a656023bedc87e2264d41fb4498b6e7e8c1ee1
Steve Bauman <steven_bauman@outlook.com>
2019-10-24 07:30:13 -0400

    Disable value input if nullable operator

--- resources/js/controllers/forms/condition-controller.js
+++ resources/js/controllers/forms/condition-controller.js
@@ -1,8 +1,31 @@
-import toaster from "../../toaster";
 import FormController from '../form-controller';
 
 export default class extends FormController {
-    after(response) {
-        toaster.fire('success', 'Success', this.data.get('message'));
+    /**
+     * Hides the value input depending on the selected operator.
+     *
+     * @param {Event} event
+     */
+    hideValueInput(event) {
+        let select = event.target;
+
+        // Get the nullabe operators from the input.
+        let nullableOperators = JSON.parse(select.dataset.nullableOperators);
+
+        // Get the value input.
+        let element = $(this.element).find('input[name=value]');
+
+        if (element.length) {
+            let input = element.get(0);
+
+            if (nullableOperators.includes(select.value)) {
+                input.disabled = true;
+                input.value = '';
+            } else {
+                input.disabled = false;
+            }
+        } else {
+            console.error('Cannot locate input: value');
+        }
     }
 }

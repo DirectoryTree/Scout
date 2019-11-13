03e36c4242abc7e46f30fee4d1df16b7bf9e2bd4
Steve Bauman <steven_bauman@outlook.com>
2019-11-13 00:10:33 -0500

    Added icon to show whether an object has been pinned

--- resources/views/domains/objects/partials/badge.blade.php
+++ resources/views/domains/objects/partials/badge.blade.php
@@ -2,5 +2,11 @@
     href="{{ route('domains.objects.show', [$object['domain']['slug'], $object['id']]) }}"
     class="badge badge-light border badge-pill"
 >
-    @include('domains.objects.partials.icon') {{ $object['name'] }}
+    @include('domains.objects.partials.icon')
+
+    {{ $object['name'] }}
+
+    @if($object->pinned)
+        <i class="ml-1 fa fa-xs fa-thumbtack"></i>
+    @endif
 </a>

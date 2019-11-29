60ca6cc439570e843297f2b5776320531512d5ca
Steve Bauman <steven_bauman@outlook.com>
2019-11-29 17:08:19 -0500

    Allow processed to be fillable

--- app/LdapScanEntry.php
+++ app/LdapScanEntry.php
@@ -9,6 +9,13 @@ class LdapScanEntry extends Model
     use IsSelfReferencing;
 
     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['processed'];
+
+    /**
      * The attributes that should be cast to native types.
      *
      * @var array

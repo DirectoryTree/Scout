f0d92008d7192f7642e12664cda6bdc82edf7f31
Steve Bauman <steven_bauman@outlook.com>
2019-12-05 12:40:22 -0500

    Working on email settings

--- app/Http/Controllers/Settings/EmailController.php
+++ app/Http/Controllers/Settings/EmailController.php
@@ -2,12 +2,47 @@
 
 namespace App\Http\Controllers\Settings;
 
+use App\Scout;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class EmailController extends Controller
 {
+    /**
+     * Displays the form for editing email settings.
+     *
+     * @return \Illuminate\View\View
+     */
     public function edit()
     {
         return view('settings.email.edit');
+    }
+
+    /**
+     * Updates the application email settings.
+     *
+     * @param Request $request
+     *
+     * @return \App\Http\ScoutResponse
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function update(Request $request)
+    {
+        $this->validate($request, [
+            'enabled' => 'boolean',
+            'port' => 'required_with:enabled',
+            'encryption' => '',
+            'username' => 'required_with:enabled',
+            'password' => 'confirmed',
+            'from_name' => 'required_with:enabled',
+            'from_address' => 'required_with:enabled',
+        ]);
+
+        setting()->set([
+            'app.email.enabled' => $request->has('enabled'),
+        ]);
+
+        return Scout::response()->notifyWithMessage('Updated settings.');
     }
 }
--- resources/views/settings/email/edit.blade.php
+++ resources/views/settings/email/edit.blade.php
@@ -3,7 +3,7 @@
 @section('title', __('Email Settings'))
 
 @section('page')
-    <form method="post" action="#" data-controller="form">
+    <form method="post" action="{{ route('settings.email.update') }}" data-controller="form">
         @csrf
         @method('patch')
 
@@ -19,6 +19,7 @@
                             form()->checkbox()
                                 ->name('enabled')
                                 ->value(true)
+                                ->checked(setting('app.email.enabled', false))
                                 ->id('enable-email')
                                 ->label(__('Enable sending email notifications'))
                                 ->data('target', 'expandable.toggle')
@@ -35,7 +36,11 @@
                                 form()->select()
                                     ->name('driver')
                                     ->options(['smtp' => 'SMTP'])
+                                    ->data('target', 'form.input')
+                                    ->data('action', 'change->form#clearError')
                             }}
+
+                            {{ form()->error()->data('input', 'driver')->data('target', 'form.error') }}
                         </div>
 
                         <div class="form-group">
@@ -44,9 +49,13 @@
                             {{
                                 form()->number()
                                     ->name('port')
-                                    ->value('')
+                                    ->value(setting('app.email.port', 587))
                                     ->placeholder('Enter port number')
+                                    ->data('target', 'form.input')
+                                    ->data('action', 'keyup->form#clearError')
                             }}
+
+                            {{ form()->error()->data('input', 'port')->data('target', 'form.error') }}
                         </div>
 
                         <div class="form-group">
@@ -55,9 +64,13 @@
                             {{
                                 form()->input()
                                     ->name('encryption')
-                                    ->value('')
+                                    ->value(setting('app.email.encryption', 'tls'))
                                     ->placeholder('Enter mail encryption')
+                                    ->data('target', 'form.input')
+                                    ->data('action', 'keyup->form#clearError')
                             }}
+
+                            {{ form()->error()->data('input', 'encryption')->data('target', 'form.error') }}
                         </div>
 
                         <div class="form-group">
@@ -68,7 +81,11 @@
                                     ->name('username')
                                     ->value('')
                                     ->placeholder('Enter username')
+                                    ->data('target', 'form.input')
+                                    ->data('action', 'keyup->form#clearError')
                             }}
+
+                            {{ form()->error()->data('input', 'username')->data('target', 'form.error') }}
                         </div>
 
                         <div class="form-group">
@@ -78,7 +95,11 @@
                                 form()->password()
                                     ->name('password')
                                     ->placeholder('Enter password')
+                                    ->data('target', 'form.input')
+                                    ->data('action', 'keyup->form#clearError')
                             }}
+
+                            {{ form()->error()->data('input', 'password')->data('target', 'form.error') }}
                         </div>
 
                         <div class="form-group">
@@ -88,7 +109,11 @@
                                 form()->password()
                                     ->name('password_confirmation')
                                     ->placeholder('Confirm above password')
+                                    ->data('target', 'form.input')
+                                    ->data('action', 'keyup->form#clearError')
                             }}
+
+                            {{ form()->error()->data('input', 'password_confirmation')->data('target', 'form.error') }}
                         </div>
 
                         <div class="form-group">
@@ -97,9 +122,13 @@
                             {{
                                 form()->input()
                                     ->name('from_name')
-                                    ->value('')
+                                    ->value(setting('app.email.from.name', 'Scout'))
                                     ->placeholder('John Doe')
+                                    ->data('target', 'form.input')
+                                    ->data('action', 'keyup->form#clearError')
                             }}
+
+                            {{ form()->error()->data('input', 'from_name')->data('target', 'form.error') }}
                         </div>
 
                         <div class="form-group">
@@ -108,19 +137,40 @@
                             {{
                                 form()->email()
                                     ->name('from_address')
-                                    ->value('')
+                                    ->value(setting('app.email.from.address'))
                                     ->placeholder('jdoe@email.com')
+                                    ->data('target', 'form.input')
+                                    ->data('action', 'keyup->form#clearError')
                             }}
+
+                            {{ form()->error()->data('input', 'from_address')->data('target', 'form.error') }}
                         </div>
                     </div>
                 </div>
             </div>
 
+            @if(setting('app.email.enabled'))
+                <div class="card-header border-bottom">
+                    <h6 class="mb-0 text-muted font-weight-bold">{{ __('Send Test Email') }}</h6>
+                </div>
+
+                <div class="card-body">
+
+
+                    <button type="button" onclick="event.preventDefault();document.getElementById('test-email-form').submit()" class="btn btn-outline-primary">
+                        Send
+                    </button>
+                </div>
+            @endif
             <div class="card-footer bg-light text-center">
                 <button type="submit" class="btn btn-primary">
                     Save
                 </button>
             </div>
         </div>
+    </form>
+
+    <form id="test-email-form" method="post" action="#" style="display: none;">>
+        @csrf
     </form>
 @endsection
